094d5775649fa50f33e8f71e394a4bf080e02c2e
vikramaditya.k.gaikwad <vikramaditya.k.gaikwad@pwc.com>
2022-06-13 22:15:51 +0530

    spring boot file upload application

---
+++ src/main/java/com/myapp/controller/UploadController.java
@@ -0,0 +1,28 @@
+package com.myapp.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+public class UploadController {
+	Map<String, Object> result = new HashMap<>();
+	/// Receive message
+	@RequestMapping("/uploadFile")
+	public Map<String, Object> upload(@RequestParam("attach")MultipartFile file) throws IOException {
+	   // File info
+	   System.out.println("File name = "  + file.getOriginalFilename());
+	   System.out.println("File type = " + file.getContentType());
+
+	   // Save to disk
+	   // file path example 1) Windows c:/, 3) Mac ~/Documents/
+	   String filePath = "C:/Desktop/";
+	   file.transferTo(new File(filePath + file.getOriginalFilename()));
+	   result.put("Success", true);
+	   return result;
+	}
+}
